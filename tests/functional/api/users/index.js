33743aa6714331c22dab737f5140f3385cbe8093
“Mark <markstaff2@gmail.com>
2022-01-06 06:44:17 +0000

    files copied over to new folder

--- tests/functional/api/users/index.js
+++ tests/functional/api/users/index.js
@@ -8,6 +8,9 @@ const expect = chai.expect;
 let db;
 let user1token;
 
+
+   
+
 describe("Users endpoint", () => {
   before(() => {
     mongoose.connect(process.env.MONGO_DB, {
@@ -27,7 +30,7 @@ describe("Users endpoint", () => {
   beforeEach(async () => {
     try {
       await User.deleteMany();
-      // Register two users
+      
       await request(api).post("/api/users?action=register").send({
         username: "user1",
         password: "test1",
@@ -58,52 +61,7 @@ describe("Users endpoint", () => {
           done();
         });
     });
+  }); 
+    
   });
 
-  describe("POST /api/users ", () => {
-    describe("For a register action", () => {
-      describe("when the payload is correct", () => {
-        it("should return a 201 status and the confirmation message", () => {
-          return request(api)
-            .post("/api/users?action=register")
-            .send({
-              username: "user3",
-              password: "test3",
-            })
-            .expect(201)
-            .expect({ msg: "Successful created new user.", code: 201 });
-        });
-        after(() => {
-          return request(api)
-            .get("/api/users")
-            .set("Accept", "application/json")
-            .expect("Content-Type", /json/)
-            .expect(200)
-            .then((res) => {
-              expect(res.body.length).to.equal(3);
-              const result = res.body.map((user) => user.username);
-              expect(result).to.have.members(["user1", "user2", "user3"]);
-            });
-        });
-      });
-    });
-    describe("For an authenticate action", () => {
-      describe("when the payload is correct", () => {
-        it("should return a 200 status and a generated token", () => {
-          return request(api)
-            .post("/api/users?action=authenticate")
-            .send({
-              username: "user1",
-              password: "test1",
-            })
-            .expect(200)
-            .then((res) => {
-              expect(res.body.success).to.be.true;
-              expect(res.body.token).to.not.be.undefined;
-              user1token = res.body.token.substring(7);
-            });
-        });
-      });
-    });
-  });
-});
