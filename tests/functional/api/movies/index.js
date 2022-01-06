3d366c3a559321b90eed0a2b8b93fa5982b58d3c
“Mark <markstaff2@gmail.com>
2022-01-06 06:44:43 +0000

    tests added for movies and users

--- tests/functional/api/movies/index.js
+++ tests/functional/api/movies/index.js
@@ -8,3 +8,129 @@ const expect = chai.expect;
 let db;
 let user2token;
 
+describe("Movies endpoint", () => {
+    before(() => {
+      mongoose.connect(process.env.MONGO_DB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      db = mongoose.connection;
+    });
+  
+    after(async () => {
+      try {
+        await db.dropDatabase();
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  
+    beforeEach(async () => {
+      try {
+        await User.deleteMany();
+        
+        await request(api).post("/api/users?action=register").send({
+          username: "user1",
+          password: "test1",
+        });
+        await request(api).post("/api/users?action=register").send({
+          username: "user2",
+          password: "test2",
+        });
+      } catch (err) {
+        console.error(`failed to Load user test Data: ${err}`);
+      }
+      return request(api)
+      .post("/api/users?action=authenticate")
+      .send({
+        username: "user2",
+        password: "test2",
+  
+      })
+      .expect(200)
+      .then((res) => {
+        expect(res.body.success).to.be.true;
+        expect(res.body.token).to.not.be.undefined;
+        user2token ="Bearer "+ res.body.token.substring(7); 
+        console.log(user2token)
+  
+    });
+  
+    });
+  
+  
+    describe("GET /api/movies/tmdb/.., returns object", () => {
+        it("should return tmdb discover movies and a status 200", (done) => {
+          request(api)
+            .get("/api/movies/tmdb/discover")
+            .set("Authorization", user2token )
+            .expect(200)
+            .end((err, res) => {
+              expect(res.body).to.be.a("object");
+              // console.log(res.body)
+              done();
+            });
+        });
+  
+     
+  
+    });
+  
+    describe("GET /api/movies/tmdb/.., returns the genres", () => {
+  
+        it("should return tmdb the tv Show genres  and a status 200", (done) => {
+          request(api)
+            .get("/api/movies/tmdb/tvGenres")
+            .set("Authorization", user2token )
+            .expect(200)
+            .end((err, res) => {
+              expect(res.body).to.be.a("object");
+              //console.log(res.body)
+              done();
+            });
+        });
+  
+    it("should return tmdb upcoming movies and a status 200", (done) => {
+          request(api)
+            .get("/api/movies/tmdb/upcoming")
+            .set("Authorization", user2token )
+            .expect(200)
+            .end((err, res) => {
+              expect(res.body).to.be.a("object");
+              //console.log(res.body)
+              done();
+            });
+        });
+    
+        it("should return tmdb top rated movies and a status 200", (done) => {
+          request(api)
+            .get("/api/movies/tmdb/top_rated")
+            .set("Authorization", user2token )
+            .expect(200)
+            .end((err, res) => {
+              expect(res.body).to.be.a("object");
+              //console.log(res.body)
+              done();
+            });
+        });
+  
+  
+  //
+  
+        it("should return tmdb movies genres  and a status 200", (done) => {
+          request(api)
+            .get("/api/movies/tmdb/movieGenres")
+            .set("Authorization", user2token )
+            .expect(200)
+            .end((err, res) => {
+              expect(res.body).to.be.a("object");
+              done();
+            });
+        });
+  
+      });
+  
+    afterEach(() => {
+      api.close(); // Release PORT 8080
+    });
+  });
